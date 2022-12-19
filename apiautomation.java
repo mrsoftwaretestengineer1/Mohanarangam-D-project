98d6ac92cc8c6002a1d1b7c5f2f3bb351b05dc88
MOHANARANGAM D <109018608+mrsoftwaretestengineer1@users.noreply.github.com>
2022-12-19 10:03:51 +0530

    Update apiautomation.java

--- apiautomation.java
+++ apiautomation.java
@@ -22,35 +22,46 @@ public class apiautomation {
 		// Step 4: Maximise the window
 			
 			driver.manage().window().maximize();
-			
+		
+		
+			//Clicks on Get method
 			driver.findElement(By.xpath("//li[@data-http='get']")).click();
 			
-						
+				//Response code		
 			driver.findElement(By.className("response-title")).getText();
+		
+			//Clicks on Post method
 			
 			driver.findElement(By.xpath("(//li[@data-id='login-successful']")).click();
-			
+			//Response code	
 			driver.findElement(By.xpath("//span[@data-key='response-code']")).getText();
-			
+		
+		
+			//Clicks on Post method
 			driver.findElement(By.xpath("//li[@data-id='login-unsuccessful']")).click();
-			
+			//Response code	
 			driver.findElement(By.xpath("//span[@class='response-code bad']")).getText();
+		        //Clicks on Get method
 			
 			driver.findElement(By.xpath("(//li[@data-id='users-single-not-found']")).click();
-			
+			//Response code	
 			driver.findElement(By.xpath("//span[@class='response-code bad']")).getText();
+		
+		               //Clicks on patch method
 					
 			driver.findElement(By.xpath("(//li[@data-id='unknown']/following-sibling::li[1]]")).click();
-			
+			//Response code	
 			driver.findElement(By.xpath("//p[@class='response-title']//span[1]")).getText();
+		
+		      //Clicks on Put method
 			
 			driver.findElement(By.xpath("//li[@data-http='put']")).click();
-			
+			//Response code	
 			driver.findElement(By.xpath("//span[@data-key='response-code']")).getText();
 			
-			
+			 //Clicks on Delete method
 			driver.findElement(By.xpath("//li[@data-id='delete']")).click();
-			
+			//Response code	
 			driver.findElement(By.xpath("//span[@data-key='response-code']")).getText();
 			
 			
